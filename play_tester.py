33e05d0c8228ac910a831806612c6133c17a0eb1
Tasha <tashachang2@gmail.com>
2022-09-20 13:16:13 -0700

    Created get_available_recs function

--- play_tester.py
+++ play_tester.py
@@ -9,10 +9,10 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 # play testing section
-print("\n-----Wave 01 test data-----")
-pp.pprint(HORROR_1)
-pp.pprint(FANTASY_1)
-pp.pprint(FANTASY_2)
+# print("\n-----Wave 01 test data-----")
+# pp.pprint(HORROR_1)
+# pp.pprint(FANTASY_1)
+# pp.pprint(FANTASY_2)
 
 # print("\n-----Wave 02 user_data-----")
 # pp.pprint(clean_wave_2_data())
@@ -21,8 +21,8 @@ pp.pprint(FANTASY_2)
 #pp.pprint(clean_wave_3_data())
 
 # Wave 04 user data
-#print("\n-----Wave 04 user_data-----")
-#pp.pprint(clean_wave_4_data())
+# print("\n-----Wave 04 user_data-----")
+# pp.pprint(clean_wave_4_data())
 
 # Wave 05 user data
 #print("\n-----Wave 05 user_data-----")
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -113,6 +113,18 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+def get_available_recs(user_data):
+    recommended_movies = []
+    host_list = user_data['subscriptions']
+    friends_watched = get_friends_unique_watched(user_data)
+
+    for movie in friends_watched:
+        if movie['host'] in host_list:
+            recommended_movies.append(movie)
+            
+    return recommended_movies
+    # add if: movie in friends_unique_watched and "host" is in "subscriptions"
+
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
